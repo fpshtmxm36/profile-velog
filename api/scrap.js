5df2d58608d1ffb3e5ba6fca7e81563930ce1ff1
fpshtmxm36 <fpshtmxm36@gmail.com>
2023-02-13 17:45:05 +0900

    230213_수정중
    *계속 스크래핑 보여주는 화면 수정중

--- api/scrap.js
+++ api/scrap.js
@@ -1,44 +1,42 @@
+const express = require('express');
+const router = express.Router();
 const axios = require("axios");
 const cheerio = require("cheerio");
-const log = console.log;
 
-const getHtml = async () => {
+const url = 'https://velog.io/@fpshtmxm36';
+
+const getHtml = async (url) => {
     try {
-        return await axios.get('https://velog.io/@fpshtmxm36');
+        return await axios.get(url);
     } catch (error) {
         console.error(error);
     }
 };
 
-module.exports = async function fetchPost() {
-    try {
-      const { data } = await getHtml();
-      return data;
-    } catch (e) {
-      throw new Error(e);
-    }
-  }
-  
-getHtml()
-    .then(html => {
-    let ulList = [];
-    const $ = cheerio.load(html.data);
-    const $bodyList = $("#root > div:nth-child(2) > div:nth-child(3) > "
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    getHtml(url)
+        .then(html => {
+            let ulList = [];
+            const $ = cheerio.load(html.data);
+
+
+            const $bodyList = $("#root > div:nth-child(2) > div:nth-child(3) > "
 				+ " div:nth-child(4) div:nth-child(3) div").children();
     
-    $bodyList.each(function(i, elem) {
-        if(i == 4){
-            return false;
-        }
-        ulList[i] = {
-            title: $(this).find('div a h2').text(),
-            url: $(this).find('div a').attr('href'),
-            date: $(this).find('div div.subinfo span:nth-child(1)').text()
-        };
-    });
+            $bodyList.each(function(i, elem) {
+                if(i == 4){
+                    return false;
+                }
+                ulList[i] = {
+                    title: $(this).find('div a h2').text(),
+                    url: $(this).find('div a').attr('href'),
+                    date: $(this).find('div div.subinfo span:nth-child(1)').text()
+                };
+            });
+            return ulList.filter(n => n.title);
+        })
+        .then(result => res.render('index', { title: result }));
+});
 
-    const data = ulList.filter(n => n.title);
-    
-    return data;
-    })
-    .then(res => log(res));
+module.exports = router;
