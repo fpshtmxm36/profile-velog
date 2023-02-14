e0250f958232ead2e49f0622e0ee6440e45f45c9
fpshtmxm36 <fpshtmxm36@gmail.com>
2023-02-14 13:27:07 +0900

    230214_scrap 4차수정
    *수정

--- api/scrap.js
+++ api/scrap.js
@@ -2,35 +2,26 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const log = console.log;
 
-const getHtml = async () => {
-    try {
-        return await axios.get('https://velog.io/@fpshtmxm36');
-    } catch (error) {
-        console.error(error);
-    }
-};
+async function getDetail() {
+    return await axios
+    .get('https://velog.io/@fpshtmxm36')
+    .then(async (data) => {
+        const $ = cheerio.load(data.data);
+        const $bodyList = $("#root > div:nth-child(2) > div:nth-child(3) > "
+            + " div:nth-child(4) div:nth-child(3) div").children();
 
-getHtml()
-    .then(html => {
-    let ulList = [];
-    const $ = cheerio.load(html.data);
-    const $bodyList = $("#root > div:nth-child(2) > div:nth-child(3) > "
-				+ " div:nth-child(4) div:nth-child(3) div").children();
-    
-    $bodyList.each(function(i, elem) {
-        if(i == 4){
-            return false;
-        }
-        ulList[i] = {
-            title: $(this).find('div a h2').text(),
-            date: $(this).find('div div.subinfo span:nth-child(1)').text()
-        };
+        $bodyList.each(function(i, elem) {
+            if(i == 4){
+                return false;
+            }
+            ulList[i] = {
+                title: $(this).find('div a h2').text(),
+                date: $(this).find('div div.subinfo span:nth-child(1)').text()
+            };
+        });
+        const content = ulList.filter(n => n.title);
+        return content;
     });
+}
 
-    const data = ulList.filter(n => n.title);
-    
-    return data;
-    })
-    .then((res) => {return res;});
-
-    module.exports = getHtml;
+module.exports = getDetail;
