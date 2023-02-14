90a27c263237d8099b33a457bb25911908998140
fpshtmxm36 <fpshtmxm36@gmail.com>
2023-02-14 11:13:34 +0900

    230214_scrap 2차수정
    *수

--- api/scrap.js
+++ api/scrap.js
@@ -1,41 +1,34 @@
-const express = require('express');
-const router = express.Router();
 const axios = require("axios");
 const cheerio = require("cheerio");
-
 const url = 'https://velog.io/@fpshtmxm36';
 
-const getHtml = async () => {
+async function scrapList() {
     try {
         return await axios.get(url);
-    } catch (error) {
-        console.error(error);
+    } catch (e) {
+      throw new Error(e);
     }
-};
+  }
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    getHtml()
-        .then(html => {
-            let ulList = [];
-            const $ = cheerio.load(html.data);
+getHtml()
+  .then(html => {
+      let ulList = [];
+      const $ = cheerio.load(html.data);
 
-            const $bodyList = $("#root > div:nth-child(2) > div:nth-child(3) > "
-				+ " div:nth-child(4) div:nth-child(3) div").children();
-    
-            $bodyList.each(function(i, elem) {
-                if(i == 4){
-                    return false;
-                }
-                ulList[i] = {
-                    title: $(this).find('div a h2').text(),
-                    url: $(this).find('div a').attr('href'),
-                    date: $(this).find('div div.subinfo span:nth-child(1)').text()
-                };
-            });
-            return ulList.filter(n => n.title);
-        })
-        .then(result => res.render('index', { title: result }));
-});
+      const $bodyList = $("#root > div:nth-child(2) > div:nth-child(3) > "
+          + " div:nth-child(4) div:nth-child(3) div").children();
 
-module.exports = router;
+      $bodyList.each(function(i, elem) {
+          if(i == 4){
+              return false;
+          }
+          ulList[i] = {
+              title: $(this).find('div a h2').text(),
+              url: $(this).find('div a').attr('href'),
+              date: $(this).find('div div.subinfo span:nth-child(1)').text()
+          };
+      });
+      return ulList.filter(n => n.title);
+  });
+
+module.exports = scrapList;
