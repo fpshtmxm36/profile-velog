1faa04c721ea7de9c2dbcc9eb87ea1a67f331c74
fpshtmxm36 <fpshtmxm36@gmail.com>
2023-02-14 11:43:02 +0900

    230214_box 3차수정
    *수정

--- src/box.js
+++ src/box.js
@@ -15,7 +15,7 @@ const createLatestCardTitle = () => {
         <g transform="translate(0, 0)">
             <text data-testid="lang-list" class="list-style" x="5" y="20">•</text>
                 <text data-testid="lang-name" x="20" y="20" class="log-title">${
-                  posts.title || "-" || posts.date
+                  posts || "-" || posts
                 }</text>
             </a>
         </g>
