aed97e48ac916fbe5db21f3c6268c6c3e77e754f
fpshtmxm36 <fpshtmxm36@gmail.com>
2023-02-14 15:43:44 +0900

    230214_scrap 15차수정
    *수정

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
-const scrapper = require("./scrap");
+const { parsing } = require("./scrap");
 const createBox = require("../src/box");
 
 module.exports = async (req, res) => {
   //const { name } = req.query;
   res.setHeader("Content-Type", "image/svg+xml");
   try {
-    const posts = await scrapper();
+    const posts = await parsing();
     return res.send(createBox(posts));
   } catch (e) {
     console.log(e);
--- api/scrap.js
+++ api/scrap.js
@@ -1,27 +1,39 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
 
-module.exports = () =>{
-    let ulList = [];
-    let bookContents = "";
-    return axios.get(`https://velog.io/@fpshtmxm36`)
-        .then(dataa=>{
-            const $ = cheerio.load(dataa.data);
-            
-            const $bodyList = $("#root > div:nth-child(2) > div:nth-child(3) > "
+const express = require('express');
+const router = express.Router();
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+var html = "";
+let ulList = [];
+async function getHTML(){
+    try{
+        console.log("getHtml");
+        return await axios.get("https://velog.io/@fpshtmxm36");
+    }catch(err){
+        console.log(err);
+    }
+}
+
+function parsing(){
+    html = getHTML();
+    const $ = cheerio.load(html.data);
+    const $bodyList = $("#root > div:nth-child(2) > div:nth-child(3) > "
 				+ " div:nth-child(4) div:nth-child(3) div").children();
     
-            $bodyList.each(function(i, elem) {
-                if(i == 4){
-                    return false;
-                }
-                ulList[i] = {
-                    title: $(this).find('div a h2').text(),
-                    date: $(this).find('div div.subinfo span:nth-child(1)').text()
-                };
-            });
+    $bodyList.each(function(i, elem) {
+        if(i == 4){
+            return false;
+        }
+        ulList[i] = {
+            title: $(this).find('div a h2').text(),
+            date: $(this).find('div div.subinfo span:nth-child(1)').text()
+        };
+    });
+    
+    //const data = ulList.filter(n => n.title);
+    
+    return ulList;
+}
 
-            return ulList;
-
-        })
-};
+module.exports = parsing;
