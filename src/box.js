4dd22e87424ec912bc4c7a45669cc0c2a2f81dee
fpshtmxm36 <fpshtmxm36@gmail.com>
2023-02-21 16:11:21 +0900

    230221_ellipsis 9차 재수정
    *재 수정

--- src/box.js
+++ src/box.js
@@ -11,7 +11,29 @@ const latestCardStyle =`<style>
     <script src="ellipsis.js"></script>
     <script>
         document.addEventListener('DOMContentLoaded', function() {
-            ${textEllipsis(324)}
+            function textEllipsis(width) {
+                let el = document.querySelector('.log-title');
+            
+                if (typeof el.getSubStringLength !== "undefined") {
+                    let text = el.textContent;
+                    var len = text.length;
+                    while (el.getSubStringLength(0, len--) > width) {
+                        el.textContent = text.slice(0, len) + "...";
+                    }
+                } else if (typeof el.getComputedTextLength !== "undefined") {
+                    while (el.getComputedTextLength() > width) {
+                        text = text.slice(0,-1);
+                        el.textContent = text + "...";
+                    }
+                } else {
+                    while (el.getBBox().width > width) {
+                        text = text.slice(0,-1);
+                        el.textContent = text + "...";
+                    }
+                }
+            }
+            
+            textEllipsis(324);
         });
     </script>`;
 
