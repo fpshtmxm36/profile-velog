c150d76e7bd80a34474f06b8899dbea1a1e2c5cd
fpshtmxm36 <fpshtmxm36@gmail.com>
2023-02-21 13:43:51 +0900

    230221_CSP 위반 수정
    *Content Security Policy (CSP) 위반 관련 수정

--- src/box.js
+++ src/box.js
@@ -45,28 +45,8 @@ const createLatestCard = (data) => {
         />
         </defs>
         ${createLatestCardBody(data)}
+        <script type="text/javascript" xlink:href="ellipsis.js" ></script>
         <script>
-        function textEllipsis(width) {
-            let el = document.querySelector('.log-title');
-
-            if (typeof el.getSubStringLength !== "undefined") {
-                let text = el.textContent;
-                var len = text.length;
-                while (el.getSubStringLength(0, len--) > width) {
-                    el.textContent = text.slice(0, len) + "...";
-                }
-            } else if (typeof el.getComputedTextLength !== "undefined") {
-                while (el.getComputedTextLength() > width) {
-                    text = text.slice(0,-1);
-                    el.textContent = text + "...";
-                }
-            } else {
-                while (el.getBBox().width > width) {
-                    text = text.slice(0,-1);
-                    el.textContent = text + "...";
-                }
-            }
-        }
         textEllipsis(324);
         </script>
     </svg>`;
