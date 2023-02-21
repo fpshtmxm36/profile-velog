995fc7ff5a3af3f54cb1a910f48997fc4582aeab
fpshtmxm36 <fpshtmxm36@gmail.com>
2023-02-21 16:24:57 +0900

    230221_ellipsis 11차 재수정
    *재 수정

---
+++ src/ellipsis.js
@@ -0,0 +1,24 @@
+function textEllipsis(width) {
+    let el = document.querySelector('.log-title');
+    let text;
+
+    if (typeof el.getSubStringLength !== "undefined") {
+        text = el.textContent;
+        var len = text.length;
+        while (el.getSubStringLength(0, len--) > width) {
+            text = text.slice(0, len) + "...";
+        }
+    } else if (typeof el.getComputedTextLength !== "undefined") {
+        while (el.getComputedTextLength() > width) {
+            text = text.slice(0,-1);
+            text = text + "...";
+        }
+    } else {
+        while (el.getBBox().width > width) {
+            text = text.slice(0,-1);
+            text = text + "...";
+        }
+    }
+
+    return text;
+}
