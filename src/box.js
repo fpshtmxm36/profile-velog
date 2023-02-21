cb4d61749024b08cfede546e4d49bdf4cea07400
fpshtmxm36 <fpshtmxm36@gmail.com>
2023-02-21 14:58:17 +0900

    230221_에러 3차 수정
    *Extra content at the end of the document 관련 수정

--- src/box.js
+++ src/box.js
@@ -27,8 +27,7 @@ const createLatestCardBody = (data) => {
 };
 
 const createLatestCard = (data) => {
-    return `
-    <meta http-equiv="Content-Security-Policy-Report-Only" content="default-src 'self'; script-src 'self'; style-src 'self'; " >
+    return `<<meta http-equiv="Content-Security-Policy" content="default-src *; style-src 'self'">
     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
         width="400" height="65" viewBox="0 0 400 65" fill="none">
         ${latestCardStyle}
